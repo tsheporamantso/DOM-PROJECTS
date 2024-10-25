3f766e2bebc9f62c1aa936859de2e92e21a4d181
tsheporamantso <tshepo.ramantso@outlook.com>
2024-10-25 12:02:35 +0200

    Add event lister to toggle links menu

--- scroll/app.js
+++ scroll/app.js
@@ -12,3 +12,19 @@
 // ********** set date ************
 const date = document.querySelector('#date');
 date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+const links = document.querySelector('.links');
+const linksContainer = document.querySelector('.links-container');
+const navToggle = document.querySelector('.nav-toggle');
+
+navToggle.addEventListener('click', () => {
+  // linksContainer.classList.toggle('show-links');
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
