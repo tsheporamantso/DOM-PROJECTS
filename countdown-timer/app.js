cbb6fbb270f7978579e6bbc84414bece42442cc3
tsheporamantso <tshepo.ramantso@outlook.com>
2024-10-30 13:38:05 +0200

    Set giveaway string dynamically

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -21,3 +21,18 @@ const weekdays = [
   'Friday',
   'Saturday',
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format');
+
+const futureDate = new Date(2024, 10, 20, 11, 30, 0);
+console.log(futureDate);
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+const date = futureDate.getDate();
+const hour = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+giveaway.textContent = `giveaway ends on  ${weekday} ${date} ${month} ${year}, at ${hour}:${minutes}am`;
