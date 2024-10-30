2dd7d458be6d4d9cd13221e6734420cf3163963a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-10-30 15:50:40 +0200

    Set countdown logic

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 const months = [
   'January',
   'February',
@@ -24,10 +25,10 @@ const weekdays = [
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format');
+const items = document.querySelectorAll('.deadline-format h4');
 
-const futureDate = new Date(2024, 10, 20, 11, 30, 0);
-console.log(futureDate);
+const futureDate = new Date(2024, 9, 31, 11, 30, 0);
+
 const year = futureDate.getFullYear();
 const month = months[futureDate.getMonth()];
 const weekday = weekdays[futureDate.getDay()];
@@ -36,3 +37,49 @@ const hour = futureDate.getHours();
 const minutes = futureDate.getMinutes();
 
 giveaway.textContent = `giveaway ends on  ${weekday} ${date} ${month} ${year}, at ${hour}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+const getRemainingTime = () => {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // 1s = 1000ms
+  // 1min = 60sec
+  // 1hr = 60min
+  // 1day = 24hrs
+
+  // values in milliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  const days = Math.floor(t / oneDay);
+  const hours = Math.floor((t % oneDay) / oneHour);
+  const minutes = Math.floor((t % oneHour) / oneMinute);
+  const seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+
+  const format = (item) => {
+    if (item < 10) {
+      return `0${item}`;
+    }
+    return item;
+  };
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `
+    <h4 class="expired">sorry the time has expired</h4>
+    `;
+  }
+};
+const countdown = setInterval(getRemainingTime, 1000);
+
+getRemainingTime();
